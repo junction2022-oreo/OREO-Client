c9bdc8fea808a75ac93de3048d7df946bea4df10
김양원 <yangwon.kim@woowahan.com>
2022-08-06 19:06:15 +0900

    chore: @types/styled-components 설치

--- src/containers/HomeContainer.tsx
+++ src/containers/HomeContainer.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  border: 1px solid black;
+`;
 
 function HomeContainer() {
   return (
-    <div>
+    <Container>
       <h4>여기는 Container 컴포넌트입니다. Container 컴포넌트는 실제 화면을 그리는 컴포넌트로 사용됩니다. Page 컴포넌트의 자식으로 부릅니다.</h4>
-    </div>
+    </Container>
   );
 }
 
