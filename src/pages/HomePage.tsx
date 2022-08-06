6691efa9479520b33b198e820c01a4f63a82426d
김양원 <yangwon.kim@woowahan.com>
2022-08-06 19:03:01 +0900

    feat: react-router-dom 설치 및 세팅

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ import HomeContainer from '../containers/HomeContainer';
 function HomePage() {
   return (
     <div>
-      <h1>여기는 Page 컴포넌트입니다. Page 컴포넌트는 라우팅 대상 컴포넌트로 사용됩니다. Container 컴포넌트를 자식으로 부릅니다.</h1>
+      <h3>여기는 Page 컴포넌트입니다. Page 컴포넌트는 라우팅 대상 컴포넌트로 사용됩니다. Container 컴포넌트를 자식으로 부릅니다.</h3>
       <HomeContainer />
     </div>
   );
